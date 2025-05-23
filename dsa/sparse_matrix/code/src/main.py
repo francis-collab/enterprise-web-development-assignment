fe178579cc2be9c554f64bfb8cdb5874ea65ca71
francis-collab <f.mutabazi1@alustudent.com>
2025-05-23 22:59:55 +0300

    Update main.py

--- dsa/sparse_matrix/code/src/main.py
+++ dsa/sparse_matrix/code/src/main.py
@@ -1,7 +1,7 @@
 # main.py
 
-from src.ile_handler import read_matrix_from_file, write_matrix_to_file
-from src.matrix_operations import add_matrices, subtract_matrices, multiply_matrices
+from dsa.sparse_matrix.code.src.file_handler import read_matrix_from_file, write_matrix_to_file
+from dsa.sparse_matrix.code.src.matrix_operations import add_matrices, subtract_matrices, multiply_matrices
 import sys
 
 def main():
